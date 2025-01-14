bb71b9669b24a7a3b61e43d2668199802c63b50d
Dikshya singh <151060176+DikshyaSingh@users.noreply.github.com>
2025-01-14 17:02:58 +0530

    java_Subarray.java

--- HackerRank-master/src/java_Subarray.java
+++ HackerRank-master/src/java_Subarray.java
@@ -5,10 +5,6 @@ import java.io.*;
 public class java_Subarray  {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT.
-        Your class should be named Solution. */
-
-
         Scanner in = new Scanner(System.in);
         int size = in.nextInt();
         int[] arr = new int[size];
