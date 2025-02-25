c8b217b895106279b984daa1b20f0a3acee89900
Dikshya singh <151060176+DikshyaSingh@users.noreply.github.com>
2025-02-25 23:55:34 +0530

    Java_Substring_Comparison.java

--- HackerRank-master/src/Java_Substring_Comparison.java
+++ HackerRank-master/src/Java_Substring_Comparison.java
@@ -20,7 +20,8 @@ public class Java_Substring_Comparison
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner scan = new Scanner(System.in);
         String s = scan.next();
         int k = scan.nextInt();
